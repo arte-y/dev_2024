d38c520b15456aa4a0f45129ebcdcad6c89e4221
arte-y <tamerylmz92@gmail.com>
2024-10-18 10:00:50 +0200

    upload cicli-loops

--- xx-Esercitazioni/04_condizioni/Program.cs
+++ xx-Esercitazioni/04_condizioni/Program.cs
@@ -45,3 +45,49 @@ else
 {
     Console.WriteLine("x e minore di 5");
 }
+
+// ESEMPIO DI SWICTH
+// esegue un blocco di codice in base al valore di una variabile
+int y = 10;
+switch (y)
+{
+    case 5;
+    Console.WriteLine("y e uguale a 5");
+    break;
+    case 10;
+    Console.WriteLine("y e uguale a 10");
+    break;
+    default:
+    Console.WriteLine("y non e ne 5 ne 10");
+    break;
+}
+
+// esempio di switch con stringhe
+string z="Ciao";
+switch (z)
+{
+    case "ciao":
+        Console.WriteLine("z e uguale a cioa");
+        break;
+    case "mondo":
+        Console.WriteLine("z u uguale a mondo");
+        break;
+    default:
+    Console.WriteLine("z non e ne ciao ne mondo");
+    break;
+}
+
+// esempio di switch con bool
+bool a = true;
+switch (a)
+{
+    case true:
+    Console.WriteLine("a e true"):
+    break;
+    case false:
+    Console.WriteLine("a e false");
+    default:
+    Console.WriteLine("a non e true ne false");
+    break;
+}
+
