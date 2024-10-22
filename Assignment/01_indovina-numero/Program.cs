67cb8cbf7a8d535dc25f3f01e2c3888be4b044aa
arte-y <tamerylmz92@gmail.com>
2024-10-22 15:07:41 +0200

    codice upload

--- Assignment/01_indovina-numero/Program.cs
+++ Assignment/01_indovina-numero/Program.cs
@@ -38,21 +38,22 @@
 // new page
 Console.Clear();
 
+Random random = new Random(); // Random e la classe che genera numeri casuali
+int numeroDaIndovinare = random.Next(1, 101); // Next e il metodo che genera un numero casuale tra 1 e 100
 
-Random random = new Random(); 
-
-int numeroDaIndovinare = random.Next(1, 101); 
+Console.Clear();
 
 Console.WriteLine("Indovina il numero (tra 1 e 100): ");
 
 int numeroInserito;
-numeroInserito =0;
 
-numeroInserito = Convert.ToInt32(Console.ReadLine());
+numeroInserito = 0; // Inizializzo a 0 per entrare nel ciclo while
 
 while (numeroInserito != numeroDaIndovinare)
-{numeroInserito = Convert.ToInt32(Console.ReadLine());
-    if (numeroDaInserito == numeroDaIndovinare)
+{
+    numeroInserito = Convert.ToInt32(Console.ReadLine());
+    
+    if (numeroInserito < numeroDaIndovinare)
     {
         Console.WriteLine("Il numero da indovinare e' maggiore");
     }
@@ -60,5 +61,9 @@ while (numeroInserito != numeroDaIndovinare)
     {
         Console.WriteLine("Il numero da indovinare e' minore");
     }
+
     Console.WriteLine("Riprova: ");
+    
 }
+
+Console.WriteLine("Hai indovinato! Il numero da indovinare era: " + numeroDaIndovinare);
