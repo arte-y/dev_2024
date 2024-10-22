01a14f48968fbfac91676aa47c2f81a9996a6554
arte-y <tamerylmz92@gmail.com>
2024-10-22 16:59:36 +0200

    version3

--- Assignment/01_indovina-numero/Program.cs
+++ Assignment/01_indovina-numero/Program.cs
@@ -36,34 +36,200 @@
 // }
 
 // new page
+
+// Console.Clear();
+
+// int tentativiMassimi =5;
+// int tentativiEffettuati = 0;
+// bool haIndovinato = false;
+
+
+
+// do
+// {
+//     Random random = new Random(); // Random e la classe che genera numeri casuali
+//     int numeroDaIndovinare = random.Next(1, 100); // Next e il metodo che genera un numero casuale tra 1 e 100
+
+//     int numeroInserito;
+//     int tentativo = 5;
+//     // numeroInserito = 0; // Inizializzo a 0 per entrare nel ciclo while
+
+//     while (tentativo > 0)
+//     {
+//         Console.WriteLine("Indovina il numero (tra 1 e 100): ");
+//         numeroInserito = Convert.ToInt32(Console.ReadLine());
+//         tentativo--;
+
+//         if (numeroDaIndovinare == numeroInserito)
+//         {
+//             Console.WriteLine($"Hai indovinato! Il numero da indovinare era: {numeroDaIndovinare}");
+//             break;
+//         }
+//         else
+//         {
+
+//             if (numeroInserito < numeroDaIndovinare)
+//             {
+//                 Console.WriteLine("Il numero da indovinare e' maggiore");
+//             }
+//             else
+//             {
+//                 Console.WriteLine("Il numero da indovinare e' minore");
+//             }
+//         }
+
+//     }
+
+//     Console.WriteLine("Game over");
+
+//     Console.Write("vuole continuare a giocare? (S/N):");
+//     string risulto = Console.ReadLine().ToLower();
+//     if (risulto == "s")
+//     {
+//         Console.WriteLine("inizia a nouvo gioco");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Cioa Caro/a");
+//         break;
+//     }
+
+// } while (true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Evde yaptigim
+
+// do
+// {
+//     int gues = 1;
+//     int guesNumber = 0;
+//     Random rdn = new Random();
+//     guesNumber = rdn.Next(1, 10); //computer give a number
+
+//     while (true)
+//     {
+//         Console.WriteLine("Please insert your guess number");
+//         string guesNumberUser = Console.ReadLine(); // user gues a number--
+//         int guesNumberUser1;
+
+//         if (!int.TryParse(guesNumberUser, out guesNumberUser1)) // una domanda! perhcè non so come posso farlo...!
+//         {
+//             Console.WriteLine("Invalid input! Please insert a valid number");
+//             continue;
+//         }
+
+//         if (guesNumber == guesNumberUser1) // user and computer gues number --
+//         {
+//             Console.WriteLine("Congratulations, you guessed correctly.");
+//             break;
+//         }
+//         else
+//         {
+//             gues++;
+//             Console.Write("You guessed wrong, try again. / ");
+//         }
+//         if (guesNumber > guesNumberUser1) // computer number > from user number
+//         {
+//             Console.WriteLine("GO-UP");
+//         }
+//         else
+//         {
+//             Console.WriteLine("GO-DOWN");
+//         }
+//     }
+//     Console.WriteLine("Game Over!");
+
+//     Console.Write("Would you like to continue the game? (y/n): ");
+//     string result = Console.ReadLine().ToLower();
+
+//     if (result == "y")
+//     {
+//         Console.WriteLine("Starting a new game ...");
+//     }
+//     else if (result == "n")
+//     {
+//         Console.WriteLine("Thank you. See you again!");
+//         break; // finish the game
+
+//     }
+//     else
+//     {
+//         Console.WriteLine("Error!");
+//         break; //invalid input. finish game
+//     }
+// } while (true);
+//evde yaptigim son
+
+
+// sunum yaptigim bu
+
 Console.Clear();
 
-Random random = new Random(); // Random e la classe che genera numeri casuali
-int numeroDaIndovinare = random.Next(1, 101); // Next e il metodo che genera un numero casuale tra 1 e 100
-
-Console.Clear();
-
-Console.WriteLine("Indovina il numero (tra 1 e 100): ");
-
-int numeroInserito;
-
-numeroInserito = 0; // Inizializzo a 0 per entrare nel ciclo while
-
-while (numeroInserito != numeroDaIndovinare)
+do
 {
-    numeroInserito = Convert.ToInt32(Console.ReadLine());
-    
-    if (numeroInserito < numeroDaIndovinare)
+    Random rnd = new Random();
+    int indovinaNumero = rnd.Next(1, 10);
+
+    int inseritoNumero = 0;
+    int tentativo = 5;
+
+    while (tentativo > 0)
     {
-        Console.WriteLine("Il numero da indovinare e' maggiore");
+        Console.WriteLine($"indovina un numero (tra 1 e 100) e Hai {tentativo - 0} tentativi.");
+        inseritoNumero = int.Parse(Console.ReadLine());
+        tentativo--;
+
+        if (inseritoNumero == indovinaNumero)
+        {
+
+            Console.WriteLine($"Complimenti! Hai indovinato il numero {indovinaNumero}");
+            break;
+        }
+        else
+        {
+            if (tentativo == 0)
+            {
+                Console.WriteLine("finito tentativo");
+                break;
+            }
+            if (inseritoNumero < indovinaNumero)
+            {
+                Console.WriteLine("maggiore");
+            }
+            else
+            {
+                Console.WriteLine("minore");
+            }
+        }
+    }
+    Console.WriteLine("Game Over");
+    Console.WriteLine("****************");
+
+    Console.Write("vuole continuare a giocare? (S/N):");
+    string risulto = Console.ReadLine().ToLower();
+    if (risulto == "s")
+    {
+        Console.WriteLine("inizia a nouvo gioco");
     }
     else
     {
-        Console.WriteLine("Il numero da indovinare e' minore");
+        Console.WriteLine("Cioa Caro/a");
+        break;
     }
 
-    Console.WriteLine("Riprova: ");
-    
-}
+} while (true);
 
-Console.WriteLine("Hai indovinato! Il numero da indovinare era: " + numeroDaIndovinare);
+// hocanin eklemesi 
+
--- Assignment/01_indovina-numero/README.md
+++ Assignment/01_indovina-numero/README.md
@@ -92,3 +92,58 @@ while (numeroInserito != numeroDaIndovinare)
 
 Console.WriteLine("Hai indovinato! Il numero da indovinare era: " + numeroDaIndovinare);
 ```
+
+## Versione 3
+
+**Obiettivo**
+Imposta un numero masssi di tentativi (ad esempio, 5). Se l'utente non indivina entro questi tentativi, ilgiovo termina.
+**Istruzioni:**
+* Imposta una variabile booleana per tenere traccia se l'utente ha indovinato.
+* Mantieni un cantatore di tentativi all'interno del ciclo while.
+* Dopo ogni tentativo, decrementa il numero di tentativi rimasti e informane l'utente
+* Se i tentativi esauriscono senza che l'utente abbia indovinato, stampa un messaggio du sconfitta e rivela il numero.
+
+**Esempio di codice**
+```csharp
+Random random = new Random(); // Random e la classe che genera numeri casuali
+int numeroDaIndovinare = random.Next(1, 101); // Next e il metodo che genera un numero casuale tra 1 e 100
+int numeroInserito;
+int tentativiMassimi = 5;  
+int tentativiEffettuati = 0;  
+bool haIndovinato = false;
+int numeroUtente = 0;
+
+Console.Clear();
+
+Console.WriteLine("Indovina il numero (tra 1 e 100). Hai 5 tentativi.");
+
+while (tentativiEffettuati < tentativiMassimi && !haIndovinato)  
+{  
+    Console.Write("Tentativo {0}: ", tentativiEffettuati + 1);  
+    
+    // numeroUtente = int.Parse(Console.ReadLine());  
+    numeroUtente = int.Parse(Console.ReadLine());
+    
+    tentativiEffettuati++;
+
+    if (numeroUtente < numeroDaIndovinare)  
+    {  
+        Console.WriteLine("Il numero da indovinare è maggiore.");  
+    }  
+    else if (numeroUtente > numeroDaIndovinare)  
+    {  
+        Console.WriteLine("Il numero da indovinare è minore.");  
+    }  
+    else  
+    {  
+        Console.WriteLine("Complimenti! Hai indovinato il numero.");  
+        haIndovinato = true;  
+    }
+
+    if (!haIndovinato && tentativiEffettuati == tentativiMassimi)  
+    {  
+        Console.WriteLine("Hai esaurito i tentativi. Il numero era " + numeroDaIndovinare + ".");  
+    }  
+}
+
+```
