434e1614fec2706e1a75deb64aa5659939d78742
Tamer Yilmaz <tamerylmz92@gmail.com>
2024-11-05 17:32:25 +0100

    Update Program.cs

--- Assignment/02_sorteggio_partecipanti_corso/Program.cs
+++ Assignment/02_sorteggio_partecipanti_corso/Program.cs
@@ -37,63 +37,58 @@ do
 // ****************************************************************************************
 
 
-Console.WriteLine("Divide squadre che vuole creare squadre da questo gruppo");
-int divisoU =int.Parse(Console.ReadLine());
+List<string> partecipanti = new List<string> { "Tamer", "Felipe", "Diego", "Ivan", "Giorgio", "Anita", "Sofia", "Andrea" };
 
+Random rnd = new Random();
 
-List<string> ListPart = new List<string>
-{"Tamer", "Andrea", "Felipe", "Ivan", "Anita", "Sofia", "Giorgio", "Diego"};
+Console.Write("inserice il numero di squadre: ");
+int numeroSquadre =int.Parse(Console.ReadLine());
 
-var rnd = new Random();
-int rndNome = rnd.Next(ListPart.Count);
+List<List<string>> squadre = new List<List<string>>();
 
-int diviso = ListPart.Count / divisoU;
-
-if (diviso%2==1)
-Console.WriteLine("diviso bene");
-else
-Console.WriteLine("fuori di gruppo uno persone");
-
-Console.WriteLine(diviso);
-// Console.WriteLine(ListPart[rndNome]);
-
-Console.WriteLine("Cioa, vuole vedere partecipante di corso? Dai iniziamo!");
-
-bool control = true;
-string control1 = string.Empty;
-do
+for (int i = 0; i < numeroSquadre; i++)
 {
-    for (int i = rndNome; i < ListPart.Count; i++)
-    {
-        Console.WriteLine($"Il nome: {ListPart[i]}");
-        // ListPart.Remove(ListPart[i]);
-        // ListPart.Remove(ListPart[rndNome]);
+  squadre.Add(new List<string>());
+}
 
-        if (i <= ListPart.Count)
-        {
-            Console.WriteLine("voule vedere altr partecipante? (S/N)");
-            control1 = Console.ReadLine().ToLower();
-        }
-        else
-        {
-            Console.WriteLine("Terminato!");
-        }
-    }
+for (int i = 0; i < partecipanti.Count; i++)
+{
+  squadre[i %numeroSquadre].Add(partecipanti[i]);
+}
 
+for (int i = 0; i < squadre.Count; i++)
+{
+  Console.WriteLine($"Squadre {i+1}: {string.Join(",",squadre[i])}");
+}
 
-} while (control1 == "s");
+// string risposta = null;
 
+// Console.Clear();
 
-
-// Console.WriteLine("Vuole vedere tutte le partecipante?(s/n)");
-// string control2 = Console.ReadLine();
-
-// if (control2 == "s")
+// do
 // {
-//     for (int i = rndNome; i < ListPart.Count; i++)
-//     {
-//         Console.WriteLine($"Il nome: {ListPart[i]}");
-//     }
+//   if (partecipanti.Count == 0)
+//   {
+//     Console.WriteLine("non ci sono piu partecipanti da estrare");
+//     break;
+//   }
+//   else
+//   {
+//     int index = rnd.Next(partecipanti.Count);
+
+//     string partecipante = partecipanti[index];
+
+//     Console.WriteLine(partecipante);
+
+//     partecipanti.RemoveAt(index);
+//   }
+
+//   Console.WriteLine("Vuoi estrare un altro partecipante? (S/N)");
+//   risposta = Console.ReadLine().ToLower();
+
+
+
+// } while (risposta == "s");
 // }
 // else
 //     Console.WriteLine("Good Bye");
