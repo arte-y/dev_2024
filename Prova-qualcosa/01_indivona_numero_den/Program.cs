b0ab2adb4e0f3e447b5348c43e73fae2983973c7
arte-y <tamerylmz92@gmail.com>
2024-12-19 18:11:59 +0100

    bug-fixed

--- Prova-qualcosa/01_indivona_numero_den/Program.cs
+++ Prova-qualcosa/01_indivona_numero_den/Program.cs
@@ -12,6 +12,7 @@ string nomeUtente = "";  // Kullanıcının adı soyadı
 
 do
 {
+  // sklfjsdkgfjsldngskdg
   // İlk olarak isim ve soyisim alınır
   Console.WriteLine("Inserisci il tuo nome e cognome:");
   nomeUtente = Console.ReadLine();
